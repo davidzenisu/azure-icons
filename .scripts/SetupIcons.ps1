0abe27650c83578a76751c5d5b98b4a0a3fe7cab
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-05-10 15:16:06 +0200

    Fixerd script to deploy icons

--- .scripts/SetupIcons.ps1
+++ .scripts/SetupIcons.ps1
@@ -1,7 +1,9 @@
 $downloadDirectory = './temp'
 $productionDirectory = './public/icons'
+$azureIconsDirectory = Join-Path $productionDirectory 'Azure_Public_Service_Icons'
 $downloadFile = Join-Path $downloadDirectory AzureIcons.zip
 
+Remove-Item -Recurse -Force $azureIconsDirectory 
 New-Item -ItemType Directory -Force -Path $downloadDirectory
 New-Item -ItemType Directory -Force -Path $productionDirectory
 Invoke-WebRequest https://arch-center.azureedge.net/icons/Azure_Public_Service_Icons_V14.zip -OutFile $downloadFile
