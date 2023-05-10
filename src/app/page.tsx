369129e79db9db44cbe35ff0f75990d60a283788
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-05-10 13:54:03 +0200

    Added exclude pattern for icon names/groups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,57 @@
 import CrawlDirectory from '@/logic/filecrawler';
 import path from 'path';
 import IconInfo from './iconInfo';
+import iconConfig from '@/configuration/icons-config.json';
 
 async function getFileList() {
   const publicDirectory = path.join(process.cwd(), 'public');
   const postsDirectory = path.join(publicDirectory, 'icons');
   const fileList = await CrawlDirectory(postsDirectory);
-  const filteredList = fileList.filter(f => f.type === '.svg');
   // fix names of files and path
-  filteredList.forEach(file => {
+  fileList.forEach(file => {
     file.path = file.path.replace(publicDirectory, '');
     const fileRegex = /\d+-icon-service-(?<Name>.*)\.svg/u;
     const fileNameInfo = fileRegex.exec(file.name)?.groups;
     let changedName = fileNameInfo?.Name ?? '';
     changedName = changedName.replace('-', ' ');
     file.name = changedName;
-
   });
+
+  const filteredList = fileList.filter(f => {
+    if (f.type !== '.svg') {
+      return false;
+    }
+    if (iconConfig?.include) {
+      if (iconConfig.include.names) {
+        const includeNames = iconConfig.include.names.some(ex => f.name.includes(ex));
+        if (!includeNames){
+          return false;
+        }
+      }
+      if (iconConfig.include.parents) {
+        const includeParents = iconConfig.include.parents.some(ex => f.parent.includes(ex));
+        if (!includeParents){
+          return false;
+        }
+      }
+    } else if (iconConfig?.exclude) {
+      if (iconConfig.exclude.names) {
+        const excludeNames = iconConfig.exclude.names.some(ex => f.name.includes(ex));
+        if (!excludeNames){
+          return false;
+        }
+      }
+      if (iconConfig.exclude.parents) {
+        const excludeParents = iconConfig.exclude.parents.some(ex => f.parent.includes(ex));
+        if (excludeParents){
+          return false;
+        }
+      }
+    }
+    return true;
+  });
+
+  console.log(filteredList.map(f => f.name));
   return filteredList;
 }
 
