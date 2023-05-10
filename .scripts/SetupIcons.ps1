e11d276c7e4f9d0e603520feebc1f22df81013e3
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-05-10 10:59:23 +0200

    Change output dir to pulic/icon

--- .scripts/SetupIcons.ps1
+++ .scripts/SetupIcons.ps1
@@ -1,8 +1,9 @@
 $downloadDirectory = './temp'
-$productionDirectory = './public'
+$productionDirectory = './public/icons'
 $downloadFile = Join-Path $downloadDirectory AzureIcons.zip
 
 New-Item -ItemType Directory -Force -Path $downloadDirectory
+New-Item -ItemType Directory -Force -Path $productionDirectory
 Invoke-WebRequest https://arch-center.azureedge.net/icons/Azure_Public_Service_Icons_V14.zip -OutFile $downloadFile
 Expand-Archive $downloadFile -DestinationPath $productionDirectory
 Remove-Item $downloadFile
